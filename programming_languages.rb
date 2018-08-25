4c55f55e9dde42132a7e3d5d4696882934046448
Sam Ashtar <samashtarm@gmail.com>
2018-08-25 04:19:43 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,4 @@
-def reformat_languages(languages)
-  languages = {
+languages = {
   :oo => {
     :ruby => {
       :type => "interpreted"
@@ -31,6 +30,18 @@ def reformat_languages(languages)
   }
 }
 
-
-
+def reformat_languages(languages)
+  new_languages = {}
+  languages.each do |type, hash|
+    hash.each do |name, attributes|
+      new_languages[name] ||= attributes
+      new_languages[name][:style] ||= []
+      new_languages[name][:style] << type
+      puts new_languages
+    end
+  end
+  new_languages
 end
+
+
+reformat_languages(languages)
