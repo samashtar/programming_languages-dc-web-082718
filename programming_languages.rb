dce927558407105f4a4c7e70a547ded32486e061
Sam Ashtar <samashtarm@gmail.com>
2018-08-25 04:18:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,33 @@
 def reformat_languages(languages)
-  # your code here
+  languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
 end
